d5f0541445c3c6673544b544c242307f537d4149
Nathalie <gnanazannathalie@gmail.com>
2025-10-04 15:02:50 +0000

    mise a jour6

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+env: {
+nœud : vrai,
+es2021 : vrai,
+},
+étend : ["eslint:recommandé"],
+parserOptions : {
+ecmaVersion : "dernière version",
+sourceType : "module",
+},
+règles: {
+"no-unused-vars": "avertir",
+"no-console": "désactivé",
+},
+};
